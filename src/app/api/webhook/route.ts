b16d98c6fac5c7c65ffde5c6c8cabec043d3383c
arquam@ali <arquamalinoor@gmail.com>
2024-10-20 12:40:25 +0530

    webhook api added

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+
+export  async function POST(req:any, res:any) {
+      // Extract data from the request body
+       console.log(req.body);
+  
+      
+      // Send a success response
+      return NextResponse.json({
+        message:"hello world"
+    })
+   
+  }
+
+  export async function GET(req:any,res:any){
+    return NextResponse.json({
+        message:"hello world"
+    })
+  }
+  
